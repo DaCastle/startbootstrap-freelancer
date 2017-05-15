5bd148c95a4d0fb316a91f1975ea336119fe5148
MAGENIC\DakotaC <DakotaC@magenic.com>
2017-05-15 11:21:02 -0500

    index.html split into angularJS directives

--- angularJS/app.js
+++ angularJS/app.js
@@ -3,6 +3,40 @@
 
     var homePage = angular.module('homePage', []);
 
+
+    angular.module('homePage').directive('myHeader', function() {
+      return {
+        templateUrl: 'angularJS/directives/header.html'
+      };
+    });
+    angular.module('homePage').directive('portfolio', function() {
+      return {
+        templateUrl: 'angularJS/directives/portfolio.html'
+      };
+    });
+    angular.module('homePage').directive('about', function() {
+      return {
+        templateUrl: 'angularJS/directives/about.html'
+      };
+    });
+    angular.module('homePage').directive('contact', function() {
+      return {
+        templateUrl: 'angularJS/directives/contact.html'
+      };
+    });
+    angular.module('homePage').directive('myFooter', function() {
+      return {
+        templateUrl: 'angularJS/directives/footer.html'
+      };
+    });
+    angular.module('homePage').directive('portfolioModals', function() {
+      return {
+        templateUrl: 'angularJS/directives/portfolioModals.html'
+      };
+    });
+    
+
+
     homePage.controller('homeController', ['$scope',
         function ($scope) {
           $scope.keyWords = "Web Developer - Problem Solver - User Experience Designer";
